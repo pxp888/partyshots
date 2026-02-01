ace91097d61ae51391fafe24ecccdfecdb9afa66
paul xavier perrine <pxperrine@gmail.com>
2026-02-01 16:10:57 +0100

    add album and photo models

--- partyshot/models.py
+++ partyshot/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Album(models.Model):
+    id = models.AutoField(primary_key=True)
+    code = models.TextField()
+    name = models.TextField()
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    thumbnail = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Photo(models.Model):
+    id = models.AutoField(primary_key=True)
+    code = models.TextField()
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    link = models.TextField()
+    tlink = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    filename = models.TextField()
+
+    def __str__(self):
+        return str(self.album.name) + ' - ' + str(self.id)
